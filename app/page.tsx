1e675f114de14fdfd4727e9f3b0e34daeb268357
lwango1 <lwangodany@gmail.com>
2026-06-10 12:38:17 +0200

    Consolidation: page unique avec Scanner + Chart + Signaux

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,17 @@
 import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import MarketOverview from "@/components/MarketOverview";
-import PredictionGame from "@/components/PredictionGame";
+import DerivChart from "@/components/DerivChart";
 import MarketScanner from "@/components/MarketScanner";
-import Converter from "@/components/Converter";
-import PriceAlert from "@/components/PriceAlert";
-import AdBanner from "@/components/AdBanner";
+import PredictionGame from "@/components/PredictionGame";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main>
-        <Hero />
-        <Features />
-        <AdBanner slot="1234567890" className="py-8 max-w-4xl mx-auto" />
-        <MarketOverview />
-        <AdBanner slot="1234567891" format="horizontal" className="py-8 mx-auto flex justify-center" />
+      <main className="pt-24">
         <MarketScanner />
+        <DerivChart />
         <PredictionGame />
-        <Converter />
-        <PriceAlert />
       </main>
       <Footer />
     </>
